63aefa287f66789fbe9c9051ad48b58a51a079e7
shreekanthbk22 <73570201+shreekanthbk22@users.noreply.github.com>
2020-12-02 20:37:40 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <>
       <div className="navbar fixed-bottom navbar-dark bg-dark justify-content-center">
-        <span className="navbar-brand">Developed by Amol Dalwai</span>
+        <span className="navbar-brand">Movie app</span>
       </div>
     </>
   );
